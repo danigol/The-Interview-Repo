6b7e52cbfb7096a050e369f44da12474ec7c148f
Danielle Golinsky <degolinsky@icloud.com>
2023-12-21 11:18:49 -0500

    Quick fix to force light mode for now and also force landscape.

--- app/src/main/java/com/daniellegolinsky/theinterviewrepo/TheInterviewActivity.kt
+++ app/src/main/java/com/daniellegolinsky/theinterviewrepo/TheInterviewActivity.kt
@@ -1,13 +1,16 @@
 package com.daniellegolinsky.theinterviewrepo
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.daniellegolinsky.theinterviewrepo.ui.coolfolks.CoolPeopleScreen
 import com.daniellegolinsky.theinterviewrepo.ui.coolfolks.CoolPeopleViewModel
@@ -18,8 +21,12 @@ class TheInterviewActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
         setContent {
-            Box(modifier = Modifier.fillMaxSize()) {
+            Box(
+                modifier = Modifier.fillMaxSize().background(Color.White)
+            ) {
                 val coolViewModel = hiltViewModel<CoolPeopleViewModel>()
                 coolViewModel.fetchCoolPeople()
                 CoolPeopleScreen(viewModel = coolViewModel, modifier = Modifier)
